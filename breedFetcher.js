46e9b9213daeb8418e54fa9dada77364ed076e66
patelheta <patelheta385@gmail.com>
2022-10-27 21:28:18 -0700

    Refactoring breedFetcher function and add unit testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,34 @@
 const request = require('request');
-const args = process.argv.slice(2);
 
-let breedType = args[0];
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedType}`, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    process.exit();
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log('Breed name is not found. Please try agian!');
-    process.exit();
-  }
-  for (let item of data) {
-    console.log(item.description);
-  }
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      process.exit();
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback('Breed name is not found. Please try agian!', null);
+      process.exit();
+    }
+    for (let item of data) {
+      callback(null, item.description);
+    }
 
-});
+  });
+};
+module.exports = { fetchBreedDescription };
+
+// const breedDescription = fetchBreedDescription('Siberian', (error, description) => {
+//   if (error) {
+//     console.log('Error fetch details:', error);
+//   } else {
+//     console.log(description);
+//   }
+// });
+// console.log(breedDescription);
+// const args = process.argv.slice(2);
+
+// let breedType = args[0];
+
+
---
+++ test/breedFetcherTest.js
@@ -0,0 +1,30 @@
+
+const { fetchBreedDescription } = require('../breedFetcher');
+const { assert } = require('chai');
+
+describe('fetchBreedDescription', () => {
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      // we expect no error for this scenario
+      assert.equal(err, null);
+
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+
+      // compare returned description
+      assert.equal(expectedDesc, desc.trim());
+
+      done();
+    });
+  });
+  it('returns a error for a invalid breed, via callback', (done) => {
+    fetchBreedDescription('mocha', (err, desc) => {
+      // we expect no error for this scenario
+      const expectedError = "Breed name is not found. Please try agian!";
+      assert.equal(expectedError, err.trim());
+
+      assert.equal(desc, null);
+
+      done();
+    });
+  });
+});
